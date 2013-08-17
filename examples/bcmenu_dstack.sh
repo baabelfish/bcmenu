7d70df6467ffe4e3e71287fa414b1bac40d3ed12
Niko Junkala <baabelfish@gmail.com>
2013-08-18 01:40:18 +0300

    Removed old example scripts

--- examples/bcmenu_dstack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-dirs|tr ' ' '\n'|tac|$DIR/../bcmenu_run
-cd $(bcmenu_result)
--- examples/bcmenu_files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-IGNORE='.git'
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-find $1|grep -v "$IGNORE"|awk '{ print length($0), $0|"sort -n" }'|cut -f2- -d' '|$DIR/../bcmenu
